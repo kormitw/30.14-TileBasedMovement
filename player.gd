450aed2d46bf9b66e7397c697535aae58ee22707
gianvalen <157687026+gianvalen@users.noreply.github.com>
2026-02-02 22:50:56 +0800

    added collisions, animations, and tile-based movement

--- player.gd
+++ player.gd
@@ -1,31 +1,78 @@
 class_name Player
 extends CharacterBody2D
 
-@export var speed : float = 50
+@export var tile_size := 16
+@export var move_speed := 50.0
 @export var animation_tree : AnimationTree
-var input
-var playback : AnimationNodeStateMachinePlayback
+# @export var speed : float = 50
 
+var input_dir = Vector2.ZERO
+var moving = false
+var target_pos = Vector2.ZERO
+
+var playback : AnimationNodeStateMachinePlayback
+	
 func _ready():
 	playback = animation_tree["parameters/playback"]
+	target_pos = global_position
 
-func _physics_process(delta: float) -> void:
-	input = Input.get_vector("left","right", "up", "down")
-	velocity = input * speed
-	move_and_slide()
-	select_animation(velocity)
+	
+func _physics_process(delta):
+
+	# for tile-based movement
+	if moving:
+
+		var distance = global_position.distance_to(target_pos)
+
+		# stop exactly at tile
+		if distance < 1:
+			global_position = target_pos
+			moving = false
+			select_animation(Vector2.ZERO)
+			return
+
+		# move slowly toward tile
+		var direction = (target_pos - global_position).normalized()
+		var motion = direction * move_speed * delta
+
+		var collision = move_and_collide(motion)
+
+		# collision
+		if collision:
+			moving = false
+			select_animation(Vector2.ZERO)
+			return
+
+		return
+
+	input_dir = Input.get_vector("left", "right", "up", "down")
+
+	if input_dir == Vector2.ZERO:
+		return
+
+	# no diagonal movement
+	if abs(input_dir.x) > abs(input_dir.y):
+		input_dir = Vector2(sign(input_dir.x), 0)
+	else:
+		input_dir = Vector2(0, sign(input_dir.y))
+
+
+	# set target tile
+	target_pos = global_position + input_dir * tile_size
+	moving = true
+
+	select_animation(input_dir)
 	update_animation_parameters()
 
-
-func select_animation(velocity):
-	if velocity == Vector2.ZERO:
+func select_animation(dir):
+	if dir == Vector2.ZERO:
 		playback.travel("Idle")
 	else:
 		playback.travel("walk")
 
 func update_animation_parameters():
-	if input == Vector2.ZERO:
+	if input_dir == Vector2.ZERO:
 		return
-		
-	animation_tree["parameters/Idle/blend_position"] = input
-	animation_tree["parameters/walk/blend_position"] = input
+
+	animation_tree["parameters/Idle/blend_position"] = input_dir
+	animation_tree["parameters/walk/blend_position"] = input_dir
